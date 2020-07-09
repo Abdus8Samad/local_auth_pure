4cc440f6bd55d7f33039787393c5666cfc1dfba2
Abdus8Samad <abdus8samad@gmail.com>
2020-07-09 21:17:23 +0530

    Server and routes Initialised

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express'),
+app = express(),
+morgan = require('morgan'),
+bcrypt = require('bcrypt'),
+path = require('path'),
+PORT = process.env.PORT || 8080,
+cookieParser = require('cookie-parser'),
+flash = require('connect-flash'),
+mongoose = require('mongoose');
+require('dotenv/config');
+
+//Connect to the DB
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(() => console.log('Connected to the DB'))
+.catch(err => console.log(err));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(flash());
+
+const indexRoutes = require('./routes/index'),
+authRoutes = require('./routes/auth');
+app.use('/',indexRoutes);
+app.use('/auth',authRoutes);
+
+app.listen(PORT,() => console.log(`Server Listening at port ${PORT}`));
